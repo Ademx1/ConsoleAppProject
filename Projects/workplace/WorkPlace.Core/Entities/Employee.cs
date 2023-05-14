05b6fa6487eed1606b996fc1986d5cb9327ae302
Ademx1 <130730588+Ademx1@users.noreply.github.com>
2023-05-14 22:33:12 +0400

    Modified Employee.cs

---
+++ Projects/workplace/WorkPlace.Core/Entities/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Xml.Linq;
+using WorkPlace.Core.Interfaces;
+
+namespace WorkPlace.Core.Entities;
+
+public class Employee : IEntity
+{
+    private static int _count = 1;
+    public int EmployeeId { get; }
+    public decimal Salary { get; set; }
+    public string EmployeeName { get; set; }
+    public string EmployeeSurname { get; set; }
+    public int DepartmentId { get; set; }
+    public object Name { get; set; }
+
+    public Employee(string employeeName, string employeeSurname, decimal salary)
+    {
+        EmployeeId = _count;
+        _count++;
+        EmployeeName = employeeName;
+        EmployeeSurname = employeeSurname;
+        Salary = salary;
+    }
+
+    
+    public Employee(string employeeName, string employeeSurname, decimal salary, int departmentId):this(employeeName, employeeSurname,salary)
+    {
+        DepartmentId = departmentId;
+    }
+    
+    public override string ToString()
+    {
+        return $"{EmployeeId}, {EmployeeName}, {EmployeeSurname}";
+    }
+}
+
