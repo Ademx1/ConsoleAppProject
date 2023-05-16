90fac0438ec72858d7f15ef2ec3d26226eb710f0
Adem Novruzlu <130730588+Ademx1@users.noreply.github.com>
2023-05-16 11:50:36 +0400

    Created WorkPlace.UI

---
+++ Projects/workplace/WorkPLace.UI/Program.cs
@@ -0,0 +1,20 @@
+﻿using WorkPlace.Business.DTOs;
+using WorkPlace.Business.Services;
+using WorkPlace.Business.DTOs;
+using WorkPlace.Business.Services;
+
+
+
+EmployeeService empservice = new EmployeeService();
+DepartmentService depservice = new DepartmentService();
+CompanyService compservice = new CompanyService();
+EmployeeDto empdto = new EmployeeDto(12300, "Kamran", "Jabiyev", 001);
+DepartmentDto depdto = new DepartmentDto("Head Department", 18, 137);
+CompanyDto compdto = new CompanyDto("Code Company");
+
+
+
+
+empservice.Create(empdto);
+depservice.Create(depdto);
+compservice.Create(compdto);
